45c7434ef5c4123b12816ce8dd49227ab3c726b0
skywalker <xsdwem7@hotmail.com>
2020-08-01 11:02:30 +0800

    export_wsl2_proxy.sh

---
+++ scripts/export_wsl2_proxy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 使用source命令执行此脚本！
+
+# Trojan-qt5默认
+port=58591
+if [ $# -eq 1 ];
+then
+    port=$1
+fi
+
+host_ip=`cat /etc/resolv.conf | grep nameserver | awk '{print $2}'`
+echo "Windows物理机IP: $host_ip"
+
+echo "代理端口: $port"
+
+export http_proxy="http://$host_ip:$port"
+
+export https_proxy="https://$host_ip:$port"
+
+echo '设置完成'
+
